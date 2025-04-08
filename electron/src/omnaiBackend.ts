328f4fb2a8c5646118994f3641226da1c1a07b5b
AKMaily <83161524+AKMaily@users.noreply.github.com>
2025-04-08 08:49:30 +0200

    Feature: Starting Backend on a free port (#13)
    * Feature: Starting Backend on a free port
    
    Problem: For v0.5.0 to start the backend with a websocket connection
    a port has to be given in the CLI.
    
    Solution: Searching for a free port by testing a free port on the
    clients device instead of setting a hardcoded port. Giving the port as
    a parameter into spawn when starting the backend.
    
    Changes:
    - Implementation of the above behavior
    - Changing the app version to 0.2.0 in the package.json as this is a new
      feature
    
    * Update : Searching for port via the OS
    
    Instead of searching for a port by incrementing the port number and
    checking if the port is available, the port is now searched by the OS.
    This decrements the needed operating time. Added a Typeguard to check if
    the received addressinfo is of type Addressinfo.
    
    * Fix: Cleaner Code as suggested
    
    Import AddressInfo from net with * .
    Unnecessary 'else' statement deleted.

--- electron/src/omnaiBackend.ts
+++ electron/src/omnaiBackend.ts
@@ -1,10 +1,32 @@
 import {spawn, ChildProcess } from "child_process"; 
 import {join} from "path"; 
-import { existsSync } from "fs-extra";
+import { existsSync } from "fs";
 import {app} from "electron"; 
+import * as net from 'net';
 
 export const omnaiscopeBackendManager = (()=> { // singelton for only one possible encapsulated instance of the backend 
     let backendProcess : ChildProcess | null = null; 
+
+    function isAddressInfo(address: string | net.AddressInfo | null): address is net.AddressInfo {
+        return typeof address === 'object' && address !== null && typeof address.port === 'number';
+    }    
+
+    async function getFreePort() : Promise<number>{
+        return new Promise((resolve, reject) => {
+            const server = net.createServer(); 
+            server.unref(); //allows the programm to exit if this is the only server running 
+            server.once("error", err => reject(err)); //first handle errors then listen to skip an internal error
+            server.listen(0, () => { // the OS gets an available port
+                const address = server.address();
+                if (!isAddressInfo(address)) { // check that server.adress is from type AddressInfo
+                    return reject(new Error('Error: Port either does not exist or address is not an AddressInfo.'));
+                }
+
+                const port = (address as net.AddressInfo).port;
+                server.close(() => resolve(port));
+            });
+        });
+    }
 
     function getBackendPath(): string {
         const exePath: string = app.isPackaged 
@@ -14,13 +36,15 @@ export const omnaiscopeBackendManager = (()=> { // singelton for only one possib
         return exePath; 
     }
 
-    function startBackend(): void {
+    async function startBackend(): Promise<void> {
         const exePath : string = getBackendPath(); 
 
+        const port : number = await getFreePort(); 
+
         if(existsSync(exePath)){
-            backendProcess = spawn(exePath, ["-w"]); 
-            console.log("Backend process started"); 
-        }
+            backendProcess = spawn(exePath, ["-w", "-p", port.toString()]); 
+            console.log(`Backend process started on port ${port}`);
+        }   
     }
 
     function stopBackend(): void {
