53370f6e4386804e3e5e783649332c9f3ae21662
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2025-04-14 10:42:31 +0200

    create initial routes setup
    Use lazy loading for the graph. For now, redirect to the graph
    route all the time. Adding more routes as we see fit.

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'graph',
+    children: [
+      {
+        path: 'main',
+        loadComponent: () =>
+          import('./graph/graph.component').then(m => m.GraphComponent)
+      }
+    ]
+  },
+  {
+  path: '**',
+  redirectTo: 'graph/main'
+}
+
+];
