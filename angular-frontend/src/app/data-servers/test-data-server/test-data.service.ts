7f97708a941003a863b9370ff5529e30cbb6a6eb
AKMaily <anna.theimann@outlook.de>
2025-05-21 10:56:17 +0200

    Showcase minimal datasource functionality
    This commit intends to show how to setup the functionality for a new
    datasource, so data is visualized in the graph.
    
    The example is the implementation of basic functionality for the DevDataServer.
    
    1. getDevices : gets the list of possible devices from the server
       provides the list in a devices signal
       This is needed otherwise the message the websocket sends would be
       empty
    2. connect : opens the websocket connection to the server
       receives and parses the data. Appends the data to the existingData
       and updates the data signal .
    
    Note: The functionality cant be used as no UI is provided to get the
    devices from the server

--- angular-frontend/src/app/data-servers/test-data-server/test-data.service.ts
+++ angular-frontend/src/app/data-servers/test-data-server/test-data.service.ts
@@ -1,13 +1,94 @@
 import { Injectable, signal } from "@angular/core";
 import { DataSource } from "../../source-selection/data-source-selection.service";
-import { DataFormat } from "../omnai-scope-server/live-data.service";
+import { DataFormat} from "../omnai-scope-server/live-data.service";
+import { HttpClient } from "@angular/common/http";
+import { inject } from "@angular/core";
 
+interface DeviceInformation {
+        UUID: string; 
+        color: {
+            r: number; 
+            g: number; 
+            b: number; 
+        };
+}
+interface DeviceOverview {
+    datastreams: DeviceInformation[]; 
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class TestDataService implements DataSource {
-    readonly data = signal<Record<string, DataFormat[]>>({}); 
 
-    connect():void{}
+    private serverURL = 'localhost:8080'; // serverURL for the testDataService 
+    readonly #httpClient = inject(HttpClient); // http Client for http requests
+    private socket: WebSocket | null = null; 
+
+    readonly data = signal<Record<string, DataFormat[]>>({});
+    readonly devices = signal<DeviceInformation[]>([]); 
+
+    getDevices(): void {
+        const url = `http://${this.serverURL}/v1/get_devices`;
+        this.#httpClient.get<DeviceOverview>(url).subscribe({
+            next: (response) => {
+            console.log("got response", response);
+            if (response.datastreams) {
+                this.devices.set(response.datastreams);
+            }
+            },
+            error: (error) => {
+            console.error('Error receiving the data:', error);
+            }
+        });
+    }
+    connect():void{
+        if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+        console.log('Websocket already connected.');
+          return;
+        }
+        const wsUrl = `ws://${this.serverURL}/v1/subscribe_ws`; 
+        this.socket = new WebSocket(wsUrl); 
+
+        this.socket.addEventListener('open', () => {
+            this.data.set({}); 
+
+            const deviceUuids = this.devices().map(device => device.UUID).join(" ");
+            this.socket?.send(deviceUuids);
+        }); 
+
+        this.socket.addEventListener('message', (event) => {
+            let parsedMessage: any; 
+            try {
+                parsedMessage = JSON.parse(event.data); 
+            }
+            catch {
+                parsedMessage = event.data; 
+            }
+
+            this.data.update(records => {
+                parsedMessage.datastreams.forEach((uuid: string, index: number) => {
+                const existingData = records[uuid] ?? [];
+
+                // Für jedes Datenobjekt eine neue Zeitreihe generieren
+                const newDataPoints = parsedMessage.data.map((row: number[]) => ({
+                    timestamp: parsedMessage.timestamp,
+                    value: row[index],
+                }));
+
+                records[uuid] = existingData.concat(newDataPoints);
+                });
+
+                return { ...records };
+                });
+        });
+        
+        this.socket.addEventListener('close', () => {
+            this.socket = null;
+        });
+
+        this.socket.addEventListener('error', (error) => {
+            console.error('WebSocket Fehler:', error);
+        });
+    }
 }
