51bb7519e2044c963c8c74b7dbf77413312ce24d
AKMaily <83161524+AKMaily@users.noreply.github.com>
2025-06-02 16:39:53 +0200

    Bug/clean backendfix (#24)
    * Fix: Electron Build + Connection to OmnAI BE
    
    Enhancement :
    - Trying to connect with the Backend works after object instantiation
    --> the port information is not available in other classes and only
    after object instantiation
    --> This could lead to timing problems when trying to reach the BE
    
    Changes:
    - backend-port Service which loads and provides the port on which the backend started
    - integration of the chosen port in the server url in live-data service
    - Init the port information as a signal that is received with the
      initialization of the app
    
    What should work now :
    - Running the app with npm start should open the correct html file
    You should see the graph and two buttons
    - Clicking on the button <load devices> should show you the connected
      OmnAI-Devices underneath.
    
    Current Problems:
    - Data from the OmnAIScopes is not shown in the graph
      The dummy data works correctly
    
    * Fix: Receiving data from backend
    
    Problem : Data from the OmnAIScope was not drawn in the graph
    
    Solution: The Backend expects the UUID from the devices that should send
    data as a string . The UUID was send as a JSON string which means it had
    quotation marks around it. The Backend expects a normal string without
    quotation marks. This was fixed by this commit.
    
    * Code Enhancement: Doc and Clean code
    
    Documentation of the Backend-port Service via a comment.
    Deletion of not needed code.
    
    * Documentation Update: omnaiBackend.ts
    
    Added comments for the functions of the omnaiscopeBackendManager in the
    omnaiBackend.ts
    This should document the functions better and make the usage of these
    functions easier.
    
    * Documentation Update and Code safety
    
    Changes:
    - Doc update for omnaiBackend.ts, backend-port.service.ts and
      app.config.ts because the descriptions where not clear or false
    - check if websocket exists before sending UUIDs to the backend
    - read out url only once from the signal when trying to get devices from the backend
    
    * Doc: MiniOmni to OmnAIScope Backend
    
    For documentation the name MiniOmni should not be used as this is easily
    changeable in the future and does not necessarily lead to the OmnAIScope
    backend. Therefor the documentation was updated to reference the
    OmnAIScope backend as OmnAIScope Backend directly.

--- electron/src/omnaiBackend.ts
+++ electron/src/omnaiBackend.ts
@@ -4,14 +4,29 @@ import { existsSync } from "fs";
 import {app} from "electron"; 
 import * as net from 'net';
 
+/**
+ * @description BackendManager handles the logic needed to start and stop the local OmnAI-Backend.
+ * It provides a function to start and stop the backend, as well as receiving the port on which the backend currently runs. 
+ * @version implements v0.5.1 of the backend interface 
+ */
 export const omnaiscopeBackendManager = (()=> { // singelton for only one possible encapsulated instance of the backend 
     let backendProcess : ChildProcess | null = null;
     let port : number = 0;  
 
+    /**
+     * @description Typeguard for the AddressInfo of a port
+     * @param address 
+     * @returns 
+     */
     function isAddressInfo(address: string | net.AddressInfo | null): address is net.AddressInfo {
         return typeof address === 'object' && address !== null && typeof address.port === 'number';
     }    
-
+    /**
+     * @description gets a free port for the backend from the OS
+     * @async
+     * @usage should be used before starting the backend 
+     * @returns 
+     */
     async function getFreePort() : Promise<number>{
         return new Promise((resolve, reject) => {
             const server = net.createServer(); 
@@ -28,7 +43,11 @@ export const omnaiscopeBackendManager = (()=> { // singelton for only one possib
             });
         });
     }
-
+    /**
+     * @description Receive the path for the OmnAIScope Backend in production and dev mode. 
+     * !!! Path is hardcoded for both cases
+     * @returns hardcoded path for production and dev mode in which the OmnAIScope Backend is currently saved 
+     */
     function getBackendPath(): string {
         const exePath: string = app.isPackaged 
         ? join(process.resourcesPath, "MiniOmni.exe") // production mode 
@@ -36,25 +55,38 @@ export const omnaiscopeBackendManager = (()=> { // singelton for only one possib
 
         return exePath; 
     }
-
+    /**
+     * @description Starts the local omnai backend on a free port given by the OS
+     *  if the exe is available in the hardcoded path and a free port is available
+     * @async
+     */
     async function startBackend(): Promise<void> {
         const exePath : string = getBackendPath(); 
 
         port = await getFreePort(); 
 
         if(existsSync(exePath)){
-            backendProcess = spawn(exePath, ["-w", "-p", port.toString()]); 
-            console.log(`Backend process started on port ${port}`);
-        }   
+            backendProcess = spawn(exePath, ["-w", "-p", port.toString()], {
+                stdio: ['ignore', 'pipe', 'pipe'],
+            });
+            // read console buffer from BE because the backend does not delete the console buffer by itself
+            // therefore the programm backend stops when the console buffer is full, temporary fix
+            backendProcess.stdout.on('data', () => {}); 
+            backendProcess.stderr.on('data', () => {});
+        }
     }
-
+    /**
+     * @description Stops the omnai backend process if the process exists 
+     */
     function stopBackend(): void {
         if(backendProcess) {
             backendProcess.kill(); 
             console.log("Backend process stopped");
         }
     }
-
+    /**
+     * @returns port of the omnaiscope backend if available , else 0
+     */
     function getPort(): number {
         return port; 
     }
