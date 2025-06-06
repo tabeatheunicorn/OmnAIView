d5a3bb7c6551a61048e065de1099679230fd8338
Lukas Adrian Kron <l.kron@lugges.eu>
2025-06-06 08:23:52 +0200

    Add Electron-Menu and change GUI (#36)
    This commit changes the Electron Menu and sets entries for Analysis and
    helper-Information. This PR depends on #35.

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -1,15 +1,27 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, ipcMain, shell, Menu, dialog} from 'electron';
 import * as path from "path";
+import * as fs from 'fs';
 import { omnaiscopeBackendManager } from './omnaiBackend';
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
+let mainWindow: BrowserWindow;
+
+function getVersionPath(): string {
+  const versionPath: string = app.isPackaged 
+    ? path.join(process.resourcesPath, "version.json")
+    : path.join(__dirname, "..", "src", "version.json")
+
+    return versionPath; 
+}
+
+const versionInfo = JSON.parse(fs.readFileSync(getVersionPath(), 'utf-8'));
+
 const createWindow = (): void => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     icon: "./images/icon",
     height: 600,
     width: 800,
@@ -22,6 +34,84 @@ const createWindow = (): void => {
   const indexPath: string = path.join(__dirname, "..", "res", "angular", "browser", "index.csr.html");
   mainWindow.loadFile(indexPath).catch(err => console.error("Fehler beim Laden der HTML-Datei:", err));
 };
+
+const menuScope = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Import',
+        click: async () => {console.log("Clicked File:Import")}
+      },
+      {
+        label: 'Export',
+        click: async () => {console.log("Clicked File:Export")}
+      },
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+Q',
+        click: () => {
+          app.quit();
+        }
+      }
+    ]
+  },
+
+    {
+  label: 'Analysis',
+  submenu: [
+    {
+      label: 'Minimum',
+      click: async () => {console.log("Clicked Analysis:Minimum")}
+    },
+    {
+      label: 'Maximum',
+      click: async () => {console.log("Clicked Analysis:Maximum")}
+    },
+    {
+      label: 'Median',
+      click: async () => {console.log("Clicked Analysis:Median")}
+    },
+    {
+      label: 'PWM',
+      click: async () => {console.log("Clicked Analysis:PWM")}
+    }
+  ]
+},
+  {
+    label: 'Help',
+    submenu: [{
+      label: 'Information',
+      click: async () => {
+        dialog.showMessageBox(mainWindow, {
+          type: 'info',
+          title: 'Information',
+          message: `electron-v.${versionInfo.electronVersion}\nangular-v.${versionInfo.angularVersion}\n${versionInfo.generatedAt}\n\nMIT © ${new Date().getFullYear()} AI-Gruppe`,
+          buttons: ['OK']
+        })
+      }
+    },
+      {
+        label: 'Support-Website',
+        click: async () => {
+          shell.openExternal("https://omnaiscope.auto-intern.de/support/")
+        }
+      },
+      {
+        label: 'Developer-Tools',
+        accelerator: 'CmdOrCtrl+I',
+        click: () => {
+          if (mainWindow) {
+            mainWindow.webContents.toggleDevTools();
+          }
+        }
+      }
+    ]
+  }
+];
+
+const menu = Menu.buildFromTemplate(menuScope);
+Menu.setApplicationMenu(menu);
 
 omnaiscopeBackendManager.startBackend();
 
