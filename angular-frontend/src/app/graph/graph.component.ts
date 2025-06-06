e96b427700e742671d58c62ae66c218529deab4d
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2025-06-06 12:24:42 +0200

    feat(x-domain modi): allow absolute and relative timestamps
    add state and UI for switiching between absolute and relative timestamps
    on the x-axis.
    
    Notes: The acutal formatting is not done in this commit.

--- angular-frontend/src/app/graph/graph.component.ts
+++ angular-frontend/src/app/graph/graph.component.ts
@@ -6,17 +6,19 @@ import {
   effect,
   inject,
   PLATFORM_ID,
+  signal,
   viewChild,
   type ElementRef
 } from '@angular/core';
-import { transition } from 'd3';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NumberValue, transition } from 'd3';
 import { axisBottom, axisLeft } from 'd3-axis';
 import { select } from 'd3-selection';
+import { timeFormat } from 'd3-time-format';
 import { DeviceListComponent } from "../omnai-datasource/omnai-scope-server/devicelist.component";
 import { ResizeObserverDirective } from '../shared/resize-observer.directive';
 import { StartDataButtonComponent } from "../source-selection/start-data-from-source.component";
 import { DataSourceService } from './graph-data.service';
-import { timeFormat } from 'd3-time-format';
 
 @Component({
   selector: 'app-graph',
@@ -24,7 +26,7 @@ import { timeFormat } from 'd3-time-format';
   templateUrl: './graph.component.html',
   providers: [DataSourceService],
   styleUrls: ['./graph.component.css'],
-  imports: [ResizeObserverDirective, JsonPipe, StartDataButtonComponent, DeviceListComponent],
+  imports: [ResizeObserverDirective, JsonPipe, StartDataButtonComponent, DeviceListComponent, MatSlideToggleModule],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GraphComponent {
@@ -64,6 +66,14 @@ export class GraphComponent {
     return `translate(${xScale.range()[0]}, 0)`;
   });
 
+  /**
+   * Signal to control the x-axis time mode. Relative starts with 0, absolute reflects the time of day the data was recorded.
+   */
+  readonly xAxisTimeMode = signal<"absolute" | "relative">("absolute");
+
+  onXAxisTimeModeToggle(checked: boolean): void {
+    this.xAxisTimeMode.set(checked ? 'relative' : 'absolute');
+  }
   updateXAxisInCanvas = effect(() => {
     if (!this.isInBrowser) return;
     const formatElapsed = timeFormat('%M.%S');
