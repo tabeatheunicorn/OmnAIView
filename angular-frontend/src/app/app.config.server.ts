31329202e8db1a24ab4b80fae1ad5f1d1eb54aca
Tabea <tabea.roethemeyer@gruppe.ai>
2025-06-13 11:58:16 +0200

    chore: update angular 19->20 (#65)
    * chore:update angular/core and angular/cli to 20
    
    Opt in to all possible migrations.
    
    Notes: all code is auto-generated. Tests will be done once https://angular.dev/update-guide?v=19.0-20.0&l=1 finished
    
    * chore:update angular/material to 20
    
    All code is autogenerated
    
    * chore: update node types
    
    As angular updated the node version that is used, the types for that node version can be updated as well

--- angular-frontend/src/app/app.config.server.ts
+++ angular-frontend/src/app/app.config.server.ts
@@ -1,16 +1,11 @@
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import {  provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 import { provideHttpClient } from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    provideServerRouting(serverRoutes),
-    provideHttpClient(),
-  ]
+  providers: [provideServerRendering(withRoutes(serverRoutes)), provideHttpClient()]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
